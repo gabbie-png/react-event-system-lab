210e4012de2805e314312ffc32a411525a4db55b
Gabriella RoseMarino <Gabriellarosemarino@gmail.com>
2021-06-16 15:54:50 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
+import React, { Component } from 'react'
+
 // Code EyesOnMe Component Here
+export default class EyesOnMe extends Component{
+    handleFocusEvent = () => {
+        console.log('Good!')
+    }
+    handleBlurEvent = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+
+    render(){
+        return (
+            
+            <button
+                onFocus={this.handleFocusEvent} onBlur={this.handleBlurEvent}
+            ></button>
+            
+        )
+
+    }
+}
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
+import React, { Component } from 'react'
 // Code Keypad Component Here
+export default class Keypad extends Component{
+
+
+    handleKeyUp = (e) =>{
+        //debugger
+        console.log('Entering password...')
+    }
+
+
+    render(){
+        return (
+            <input type="password" onKeyUp={(e)=>this.handleKeyUp(e)} />
+        )
+    }
+}
